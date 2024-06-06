d2f82b458f9d3f04b7850c1b3c9c7bccec98707b
Maxime <maxime.britto@hotmail.com>
2024-06-06 19:45:55 +0200

    Pierre feuille ciseau add

--- websocket/backend/index.js
+++ websocket/backend/index.js
@@ -3,50 +3,175 @@ import http from 'http';
 import ip from 'ip';
 import { Server } from 'socket.io';
 import cors from 'cors';
+
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught error', err);
+    process.exit(1); //mandatory (as per the Node.js docs)
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    process.exit(1);
+});
+
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
 const io = new Server(server, {
     cors: {
         origin: '*',
-        }
-})
-app.use(cors())
+    }
+});
+
+app.use(cors());
+
+let rooms = {};
+let players = {};
+let scores = {};
+let pseudos = {};
+
 app.get('/', (req, res) => {
-    res.json('ip address: http://' + ip.address()+':'+PORT);    
+    res.json('ip address: http://' + ip.address() + ':' + PORT);    
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.broadcast.emit('user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        socket.broadcast.emit('user disconnected');
-    });
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('message', msg);
-    });
-    
-    socket.on('room', (room, msg) => {
-        console.log('room: ' + room + ' message: ' + msg);
-        io.to(room).emit('message', msg);
+        handlePlayerLeave(socket);
     });
 
-    socket.on('join', (room) => {
-        console.log('join room: ' + room);
+    socket.on('join', (room, pseudo) => {
+        console.log(`${pseudo} joined room: ${room}`);
         socket.join(room);
-        io.to(room).emit('join', room);
+
+        if (!rooms[room]) {
+            rooms[room] = [];
+        }
+        if (!players[room]) {
+            players[room] = {};
+        }
+        if (!scores[room]) {
+            scores[room] = {};
+        }
+        if (!pseudos[room]) {
+            pseudos[room] = {};
+        }
+        scores[room][socket.id] = 0;
+        pseudos[room][socket.id] = pseudo;
+
+        players[room][socket.id] = null;
+        io.to(room).emit('join', { room, pseudo });
+
+        if (Object.keys(players[room]).length === 2) {
+            io.to(room).emit('start_game', pseudos[room]);
+        }
     });
+
     socket.on('leave', (room) => {
         console.log('leave room: ' + room);
-        socket.leave(room);
-        io.to(room).emit('leave', room);
+        handlePlayerLeave(socket, room);
     });
-})
 
+    socket.on('rps_choice', (room, choice) => {
+        console.log(`Player ${pseudos[room][socket.id]} in room ${room} chose ${choice}`);
+        if (players[room]) {
+            players[room][socket.id] = choice;
+        }
+
+        const playerIds = Object.keys(players[room]);
+        if (playerIds.length === 2) {
+            const player1 = playerIds[0];
+            const player2 = playerIds[1];
+            const choice1 = players[room][player1];
+            const choice2 = players[room][player2];
+
+            if (choice1 && choice2) {
+                let result;
+                if (choice1 === choice2) {
+                    result = 'Draw';
+                } else if (
+                    (choice1 === 'rock' && choice2 === 'scissors') ||
+                    (choice1 === 'scissors' && choice2 === 'paper') ||
+                    (choice1 === 'paper' && choice2 === 'rock')
+                ) {
+                    result = `Player ${pseudos[room][player1]} wins this round!`;
+                    scores[room][player1] += 1;
+                } else {
+                    result = `Player ${pseudos[room][player2]} wins this round!`;
+                    scores[room][player2] += 1;
+                }
+
+                io.to(room).emit('rps_result', { result, scores: getScoreBoard(room, pseudos[room], scores[room]) });
+                players[room][player1] = null;
+                players[room][player2] = null;
+
+                if (scores[room][player1] === 2) {
+                    io.to(player1).emit('game_end', 'Gagné');
+                    io.to(player2).emit('game_end', 'Perdu');
+                    resetGame(room, false); // Reset game without resetting pseudos
+                } else if (scores[room][player2] === 2) {
+                    io.to(player2).emit('game_end', 'Gagné');
+                    io.to(player1).emit('game_end', 'Perdu');
+                    resetGame(room, false); // Reset game without resetting pseudos
+                } else {
+                    io.to(room).emit('next_round');
+                }
+            } else {
+                const waitingPlayer = choice1 ? pseudos[room][player2] : pseudos[room][player1];
+                io.to(room).emit('waiting', `En attente du choix de ${waitingPlayer}`);
+            }
+        }
+    });
+
+    socket.on('reset_game', (room) => {
+        resetGame(room, true); // Reset game and reset scores
+        io.to(room).emit('start_game', pseudos[room]);
+    });
+
+    socket.on('message', (data) => {
+        const room = Object.keys(socket.rooms).find(r => r !== socket.id);
+        io.to(room).emit('message', data);
+    });
+
+    const handlePlayerLeave = (socket, room = null) => {
+        for (const r in players) {
+            if (room && room !== r) continue;
+            if (players[r] && players[r][socket.id]) {
+                const pseudo = pseudos[r][socket.id];
+                delete players[r][socket.id];
+                delete scores[r][socket.id];
+                delete pseudos[r][socket.id];
+                io.to(r).emit('user disconnected', socket.id);
+                io.to(r).emit('stop_game', `Le joueur ${pseudo} a quitté`);
+            }
+        }
+    };
+});
+
+const getScoreBoard = (room, pseudos, scores) => {
+    if (!pseudos || !scores) return {};
+    const playerIds = Object.keys(pseudos);
+    if (playerIds.length < 2) return {};
+
+    return {
+        [pseudos[playerIds[0]]]: scores[playerIds[0]] || 0,
+        [pseudos[playerIds[1]]]: scores[playerIds[1]] || 0
+    };
+};
+
+const resetGame = (room, resetScores = false) => {
+    players[room] = {};
+    if (resetScores) {
+        for (const playerId in scores[room]) {
+            scores[room][playerId] = 0;
+        }
+    }
+    io.to(room).emit('reset_game');
+};
 
 server.listen(PORT, () => {
-    console.log('Server ip : http://' +ip.address() +":" + PORT);
-})
-
+    console.log('Server ip : http://' + ip.address() + ":" + PORT);
+});
--- websocket/front/script.js
+++ websocket/front/script.js
@@ -1,26 +1,115 @@
-let room = '';
-let socketid = '';
-const roomArea = document.querySelector('#room');
-const messageArea = document.querySelector('#message');
 const socket = io('http://localhost:3000');
+let room = '';
+let pseudo = '';
+
 socket.on('connect', () => {
     console.log('Connected');
-});
-socket.on('message', (data) => {
-    console.log(data);
-    document.querySelector('.data').innerText = data;
-});
-socket.on('disconnect', () => {
-    console.log('Disconnected');
+    if (room && pseudo) {
+        socket.emit('join', room, pseudo);
+    }
 });
 
-let send = () => {
-    console.log(messageArea.value);
-    //socket.emit('message', `message : ${messageArea.value}`);
-    socket.emit('room', roomArea.value, `Message room ${roomArea.value} : ${messageArea.value}`);
-}
-roomArea.addEventListener('change', (e) => {
-    socket.emit('leave', room);
-    socket.emit('join', e.target.value);
-    room = e.target.value;
+socket.on('rps_result', ({ result, scores }) => {
+    console.log(result);
+    document.querySelector('#game-result').innerText = result;
+    document.querySelector('#waiting-message').innerText = ''; // Clear waiting message
+    updateScoreBoard(scores);
 });
+
+socket.on('start_game', (pseudos) => {
+    console.log('Game started! Both players are ready.');
+    document.querySelector('#game').style.display = 'block';
+    document.querySelector('#join-room').style.display = 'none';
+    document.querySelector('#game-result').innerText = 'Game started! Make your choice.';
+    document.querySelector('#waiting-message').innerText = ''; // Clear waiting message
+    document.querySelector('#replay').style.display = 'none';
+    updatePlayerNames(pseudos);
+    updateScoreBoard({ [pseudos[Object.keys(pseudos)[0]]]: 0, [pseudos[Object.keys(pseudos)[1]]]: 0 });
+});
+
+socket.on('next_round', () => {
+    document.querySelector('#waiting-message').innerText = ''; // Clear waiting message
+});
+
+socket.on('game_end', (result) => {
+    console.log(result);
+    document.querySelector('#game-result').innerText = result;
+    document.querySelector('#waiting-message').innerText = ''; // Clear waiting message
+    document.querySelector('#replay').style.display = 'block';
+});
+
+socket.on('waiting', (message) => {
+    console.log(message);
+    document.querySelector('#waiting-message').innerText = message;
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected');
+    resetUI();
+});
+
+socket.on('user disconnected', (socketId) => {
+    console.log('User disconnected:', socketId);
+    resetUI();
+});
+
+socket.on('stop_game', (message) => {
+    console.log(message);
+    document.querySelector('#game-result').innerText = message;
+    resetUI();
+});
+
+function joinRoom() {
+    room = document.querySelector('#room').value;
+    pseudo = document.querySelector('#pseudo').value;
+    if (room && pseudo) {
+        socket.emit('join', room, pseudo);
+        document.querySelector('#joinButton').style.display = 'none';
+        document.querySelector('#leaveButton').style.display = 'block';
+    } else {
+        alert('Please enter a room and a pseudo');
+    }
+}
+
+function leaveRoom() {
+    socket.emit('leave', room);
+    resetUI();
+}
+
+function choose(choice) {
+    socket.emit('rps_choice', room, choice);
+    document.querySelector('#waiting-message').innerText = 'En attente du choix de l\'autre joueur';
+}
+
+function replay() {
+    socket.emit('reset_game', room);
+}
+
+function resetUI() {
+    document.querySelector('#joinButton').style.display = 'block';
+    document.querySelector('#leaveButton').style.display = 'none';
+    document.querySelector('#join-room').style.display = 'block';
+    document.querySelector('#game').style.display = 'none';
+    document.querySelector('#waiting-message').innerText = '';
+    document.querySelector('#score-board').innerHTML = '';
+    document.querySelector('#game-result').innerText = '';
+    document.querySelector('#player1-name').innerText = 'Player 1';
+    document.querySelector('#player1-score').innerText = '0';
+    document.querySelector('#player2-name').innerText = 'Player 2';
+    document.querySelector('#player2-score').innerText = '0';
+    room = '';
+    pseudo = '';
+}
+
+function updatePlayerNames(pseudos) {
+    const playerIds = Object.keys(pseudos);
+    document.querySelector('#player1-name').innerText = pseudos[playerIds[0]];
+    document.querySelector('#player2-name').innerText = pseudos[playerIds[1]];
+}
+
+const updateScoreBoard = (scores) => {
+    const player1Name = document.querySelector('#player1-name').innerText;
+    const player2Name = document.querySelector('#player2-name').innerText;
+    document.querySelector('#player1-score').innerText = scores[player1Name] || 0;
+    document.querySelector('#player2-score').innerText = scores[player2Name] || 0;
+};
